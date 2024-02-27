fdb9d83b083282ff6210005fe7dda7a39687b0dc
hoi-82 <tmddus828@gmail.com>
2024-02-27 22:21:56 +0900

    Initial commit

---
+++ src/main/java/com/example/login/loginexam/LoginexamApplication.java
@@ -0,0 +1,14 @@
+package com.example.login.loginexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class LoginexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginexamApplication.class, args);
+	}
+
+}
